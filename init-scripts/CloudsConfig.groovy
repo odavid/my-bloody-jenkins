0cb6b0ca55d3cb0aafd1ebea796a2a0d31c26566
odavid <ohad.david@gmail.com>
2017-10-05 21:38:33 +0300

    Docker cloud working

---
+++ init-scripts/CloudsConfig.groovy
@@ -0,0 +1,86 @@
+import hudson.slaves.JNLPLauncher
+import com.nirima.jenkins.plugins.docker.DockerCloud
+import com.nirima.jenkins.plugins.docker.DockerTemplate
+import com.nirima.jenkins.plugins.docker.DockerTemplateBase
+import com.nirima.jenkins.plugins.docker.DockerImagePullStrategy
+import com.nirima.jenkins.plugins.docker.launcher.DockerComputerJNLPLauncher
+import com.nirima.jenkins.plugins.docker.strategy.DockerOnceRetentionStrategy
+import hudson.model.Node
+import jenkins.model.Jenkins
+
+
+def dockerCloud(config){
+    config.with{
+        return new DockerCloud(
+            id,
+            templates?.collect{ temp ->
+                def dockerComputerJNLPLauncher = new DockerComputerJNLPLauncher(
+                    new JNLPLauncher(
+                        temp.tunnel,
+                        temp.vmargs
+                    )
+                )
+                dockerComputerJNLPLauncher.setUser(temp.user ?: 'jenkins')
+                def dockerTemplate = new DockerTemplate(
+                    new DockerTemplateBase(
+                        temp.image,
+                        temp.dns?.join(' '),
+                        temp.network,
+                        temp.command,
+                        temp.volumes?.join(' '),
+                        temp.volumesFrom?.join(' '),
+                        temp.environment?.collect{k,v -> "${k}=${v}"}.join("\n"),
+                        null, // temp.lxcConfString,
+                        temp.hostname,
+                        temp.memory?.toInteger(),
+                        temp.memorySwap?.toInteger(),
+                        temp.cpu?.toInteger(),
+                        temp.ports?.join(' '),
+                        temp.bindAllPorts?.toBoolean() ?: false,
+                        temp.privileged?.toBoolean() ?: false,
+                        temp.tty?.toBoolean() ?: false,
+                        temp.macAddress
+                    ),
+                    temp.labels?.join(' '),
+                    temp.remoteFs,
+                    temp.remoteFsMapping,
+                    "" //temp.instanceCapStr,
+                )
+                dockerTemplate.mode = Node.Mode.EXCLUSIVE
+                dockerTemplate.numExecutors = 1
+                dockerTemplate.launcher = dockerComputerJNLPLauncher
+                dockerTemplate.removeVolumes = temp.removeVolumes ? temp.removeVolumes.toBoolean() : false
+                return dockerTemplate
+            },
+            serverUrl,
+            "", //containerCapStr,
+            0, //connectTimeout,
+            0, //readTimeout,
+            credentialsId,
+            null //version,
+        )
+    }
+}
+
+def setup(config){
+    config = config ?: [:]
+    def clouds = config.collect{k,v ->
+        def cloudConfig = [id: k] << v
+        switch(v.type){
+            case 'docker':
+                return dockerCloud(cloudConfig)
+        }
+    }.grep().each{ cloud ->
+        println "cloud: ${cloud}"
+        def old = Jenkins.instance.clouds.find{ it.name == cloud.name}
+        println "old: ${old}"
+        if(old){
+            Jenkins.instance.clouds.remove(old)
+        }
+        println "adding cloud"
+        Jenkins.instance.clouds.add(cloud)
+    }
+    Jenkins.instance.save()
+}
+
+return this
--- init-scripts/init-jenkins.groovy
+++ init-scripts/init-jenkins.groovy
@@ -2,9 +2,11 @@ def sharedMethods = evaluate(new File("/var/jenkins_home/init.groovy.d/SharedMet
 def generalConfig = evaluate(new File("/var/jenkins_home/init.groovy.d/GeneralConfig.groovy"))
 def credsConfig = evaluate(new File("/var/jenkins_home/init.groovy.d/CredsConfig.groovy"))
 def securityConfig = evaluate(new File("/var/jenkins_home/init.groovy.d/SecurityConfig.groovy"))
+def cloudsConfig = evaluate(new File("/var/jenkins_home/init.groovy.d/CloudsConfig.groovy"))
 
 def jenkinsConfig = sharedMethods.loadYamlConfig('/etc/jenkins-config.yml')
 
 generalConfig.setup(jenkinsConfig.general)
 credsConfig.setup(jenkinsConfig.credentials)
 securityConfig.setup(jenkinsConfig.security)
+cloudsConfig.setup(jenkinsConfig.clouds)
