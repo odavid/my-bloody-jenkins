91babb478e673060a1c7a7f5c17c5c6d64b753ef
odavid <ohad.david@gmail.com>
2017-10-05 18:17:48 +0300

    Removed loadProperties

--- init-scripts/SharedMethods.groovy
+++ init-scripts/SharedMethods.groovy
@@ -1,14 +1,5 @@
 import org.yaml.snakeyaml.Yaml
 
-def loadProperties(){
-    Properties envProperties = new Properties()
-    File propertiesFile = new File('/tmp/jenkins-env.properties')
-    propertiesFile.withInputStream {
-        envProperties.load(it)
-    }
-    envProperties    
-}
-
 def loadYamlConfig(filename){
     return new File(filename).withReader{
         new Yaml().load(it)
