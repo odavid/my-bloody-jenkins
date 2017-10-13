4b19f0792fdb9574b4ba42b9178eb4aa2d20c752
odavid <ohad.david@gmail.com>
2017-10-13 23:42:04 +0300

    fixed scm in dsl

--- example/jobs/seed/DslScript.groovy
+++ example/jobs/seed/DslScript.groovy
@@ -1,7 +1,9 @@
 pipelineJob('sample-job-created-by-seed'){
     definition{
         cpsScm{
-            git('https://github.com/odavid/my-bloody-jenkins.git', 'kubernetes-example')
+            scm{
+                git('https://github.com/odavid/my-bloody-jenkins.git', 'kubernetes-example')
+            }
             scriptPath('example/jobs/sample-job/Jenkinsfile')
         }
     }
