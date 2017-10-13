6d9529f0eb5718dcc4cf536f4bce63dc9a6ef759
odavid <ohad.david@gmail.com>
2017-10-13 22:58:10 +0300

    Added sample-job

--- example/jobs/seed/dsl-script.groovy
+++ example/jobs/seed/dsl-script.groovy
@@ -2,7 +2,7 @@ pipelineJob('sample-job-created-by-seed'){
     definition{
         cpsScm{
             git('https://github.com/odavid/my-bloody-jenkins.git', 'kubernetes-example')
-            scriptPath('example/kubernetes/jobs/sample-job/Jenkinsfile')
+            scriptPath('example/jobs/sample-job/Jenkinsfile')
         }
     }
 }
