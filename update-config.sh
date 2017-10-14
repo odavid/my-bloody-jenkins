dfa69655a32ff9ebfdb78c787be81e4abf4eade8
odavid <ohad.david@gmail.com>
2017-10-14 21:40:24 +0300

    update-config.sh

--- update-config.sh
+++ update-config.sh
@@ -2,6 +2,11 @@
 
 script_dir=$(cd $(dirname "$0"); pwd)
 token_file=/tmp/.api-token
+AUTH_ARG=""
+
+if [ -f $token_file ]; then
+    AUTH_ARG="-auth $(cat $token_file)"
+fi
 
 if [ ! -f $script_dir/jenkins-cli.jar ]; then
     curl -o $script_dir/jenkins-cli.jar http://localhost:8080/jnlpJars/jenkins-cli.jar
@@ -9,7 +14,6 @@ fi
 
 echo "Updating Jenkins Configuration"
 java -jar $script_dir/jenkins-cli.jar \
-    -s http://localhost:8080/ \
-    -auth "$(cat $token_file)" \
+    -s http://localhost:8080/ $AUTH_ARG \
     groovy = < /var/jenkins_home/init.groovy.d/JenkinsConfigLoader.groovy
 echo "Jenkins Configuration Updated"
