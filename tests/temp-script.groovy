b3cc7dbac77cf71bea39b5172b8acd40b6796439
odavid <ohad.david@gmail.com>
2017-10-15 22:30:11 +0300

    starting with sanity

--- tests/temp-script.groovy
+++
@@ -1,4 +0,0 @@
-jenkins.model.Jenkins.instance.pluginManager.activePlugins.sort{it.shortName}.each{
-	out.println it.shortName + ':' + it.version
-}
-assert jenkins.model.Jenkins.instance.pluginManager.failedPlugins.empty
