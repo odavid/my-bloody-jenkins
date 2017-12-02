74357b52b3941c73beb09b1e0f1548be0769d10a
odavid <ohad.david@gmail.com>
2017-12-03 00:14:57 +0200

    Added anchor-container-scanner and updated plugins

--- tests/groovy/SanityTest.groovy
+++ tests/groovy/SanityTest.groovy
@@ -4,6 +4,15 @@ def printActivePlugins(){
 	}
 }
 
+def printLatestVersionsOfPlugins(){
+	def updateSite = jenkins.model.Jenkins.getInstance().getUpdateCenter().getById('default')
+	jenkins.model.Jenkins.instance.pluginManager.activePlugins.sort{it.shortName}.each{
+		def p = updateSite.getPlugin(it.shortName)
+		def version = p ? p.version : it.version
+		out.println "${it.shortName}:${version}"
+	}
+}
+
 def testNoFailedPlugins(){
 	assert jenkins.model.Jenkins.instance.pluginManager.failedPlugins.empty : "There are failed plugins"
 }
