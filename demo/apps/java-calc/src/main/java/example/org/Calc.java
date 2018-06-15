2cd59692281e2ebc08d75f8ad2715e0d09858c0b
odavid <ohad.david@gmail.com>
2018-06-15 11:45:05 +0300

    Java calc

---
+++ demo/apps/java-calc/src/main/java/example/org/Calc.java
@@ -0,0 +1,8 @@
+public class Calc{
+    public int add(int a, int b){
+        return a + b;
+    }
+    public int mul(int a, int b){
+        return a * b;
+    }
+}
---
+++ demo/apps/java-calc/src/test/java/example/org/CalcTest.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class CalcTest{
+    private Calc calc;
+
+    @Before
+    public void setUp(){
+        calc = new Calc();
+    }
+
+    @Test
+    public void testAdd(){
+        assertEquals(calc.add(1, 1), 2);
+    }
+
+    @Test
+    public void testMul(){
+        assertEquals(calc.add(2, 2), 4);
+    }
+}
