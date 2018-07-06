9d215da448cf7d9a7f67bce24c5f325561004a96
odavid <ohad.david@gmail.com>
2018-07-06 20:36:32 +0300

    tests for deep merge of configuration

---
+++ tests/groovy/deep-merge/AssertCredsFromDir32.groovy
@@ -0,0 +1,12 @@
+def globalDomain = com.cloudbees.plugins.credentials.domains.Domain.global()
+def currentCreds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+    com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl,
+    jenkins.model.Jenkins.instance,
+    null,
+    null
+)
+def cred = currentCreds.find{it.id == 'git-user-pass'}
+assert cred
+assert cred.username == "username-dir3-2"
+assert cred.password.toString() == "password-dir3-2"
+
